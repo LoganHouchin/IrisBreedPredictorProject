b2793959abe7802cc1e1cb673d582de472afa95d
Logan Houchin <Loghouch@iu.edu>
2021-04-02 12:55:00 -0400

    Prediction

--- prediction.py
+++ prediction.py
@@ -1,7 +1,7 @@
 from joblib import load
 import numpy as np
 
-model = ("Iris_Breed_Predictor.iypnb")
+model = ("Iris_model.pkl")
 
 def predPrint(id):
    print("Prediction: " + model.prediction(id))
