51a00527272a7aba15850fb10991385a921b53a0
Logan Houchin <Loghouch@iu.edu>
2021-04-09 13:25:30 -0400

    Fixing prediction.py

--- prediction.py
+++ prediction.py
@@ -1,10 +1,14 @@
 from joblib import load
 import numpy as np
 
+iris_data = datasets.load_iris()
+class_names = iris_data.target_names
+
 def predPrint(id):
    model = load('Iris_model.pkl')
    dummy = np.array(id)
    Dummy = dummy.reshape(1,-1)
    prediction = model.predict(Dummy)
-   str = ["Prediction: ",prediction]
+   Pred = class_names[prediction]
+   str = ["Prediction: ",Pred]
    return str
